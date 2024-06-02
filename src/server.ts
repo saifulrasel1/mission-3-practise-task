18396b937fda0bef578728508ca0e60b40cb98a3
saiful rasel <saifulrox11223344@gmail.com>
2024-06-03 04:59:52 +0600

    alf

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import app from "./app"
-import mongoose from "mongoose"
-import config from "./config"
+import app from "./app";
+import mongoose from "mongoose";
+import config from "./config";
 
-
-
-  async function main() {
+async function main() {
+  try {
     await mongoose.connect(config.mongodb_uri as string);
-    
+
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
-      
-    })
-    
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
   }
-  main()
+}
+main();
